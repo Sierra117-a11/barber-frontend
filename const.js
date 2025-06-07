99d420a9153b4b7705cb84eaa89c88ac58b3f4bd
julian_117 <venegasjulian04@gmail.com>
2025-06-06 21:09:22 -0500

    Update const.js

--- const.js
+++ const.js
@@ -4,6 +4,6 @@ function getServerUrl(){
     if(ENV){
         return 'http://127.0.0.1:8000'
     }else{
-        return 'https://barber-backend-5ixt.onrender.com/'
+        return 'https://barber-backend-5ixt.onrender.com'
     }
 }
